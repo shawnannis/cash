cc6ceb4f3f29841403f2a06992305d19bc1afd43
Shawn <shawnannis@yahoo.com>
2022-03-23 09:23:17 -0400

    initial commit

---
+++ Annis_Cash4Life_FA14/Form1.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnQuickPick = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblOutput01 = New System.Windows.Forms.Label()
+        Me.lblOutput02 = New System.Windows.Forms.Label()
+        Me.lblOutput03 = New System.Windows.Forms.Label()
+        Me.lblOutput04 = New System.Windows.Forms.Label()
+        Me.lblOutput05 = New System.Windows.Forms.Label()
+        Me.lblOutput06 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnQuickPick
+        '
+        Me.btnQuickPick.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnQuickPick.Location = New System.Drawing.Point(326, 25)
+        Me.btnQuickPick.Name = "btnQuickPick"
+        Me.btnQuickPick.Size = New System.Drawing.Size(106, 48)
+        Me.btnQuickPick.TabIndex = 0
+        Me.btnQuickPick.Text = "Quick Pick"
+        Me.btnQuickPick.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(231, 101)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(292, 56)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Choose 5 numbers from 1 to 60   Choose 1 CashBall number from 1 to 4"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblOutput01
+        '
+        Me.lblOutput01.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput01.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOutput01.Location = New System.Drawing.Point(108, 187)
+        Me.lblOutput01.Name = "lblOutput01"
+        Me.lblOutput01.Size = New System.Drawing.Size(67, 35)
+        Me.lblOutput01.TabIndex = 2
+        Me.lblOutput01.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblOutput02
+        '
+        Me.lblOutput02.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput02.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOutput02.Location = New System.Drawing.Point(201, 187)
+        Me.lblOutput02.Name = "lblOutput02"
+        Me.lblOutput02.Size = New System.Drawing.Size(67, 35)
+        Me.lblOutput02.TabIndex = 3
+        Me.lblOutput02.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblOutput03
+        '
+        Me.lblOutput03.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput03.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOutput03.Location = New System.Drawing.Point(294, 187)
+        Me.lblOutput03.Name = "lblOutput03"
+        Me.lblOutput03.Size = New System.Drawing.Size(67, 35)
+        Me.lblOutput03.TabIndex = 4
+        Me.lblOutput03.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblOutput04
+        '
+        Me.lblOutput04.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput04.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOutput04.Location = New System.Drawing.Point(390, 187)
+        Me.lblOutput04.Name = "lblOutput04"
+        Me.lblOutput04.Size = New System.Drawing.Size(67, 35)
+        Me.lblOutput04.TabIndex = 5
+        Me.lblOutput04.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblOutput05
+        '
+        Me.lblOutput05.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput05.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOutput05.Location = New System.Drawing.Point(483, 187)
+        Me.lblOutput05.Name = "lblOutput05"
+        Me.lblOutput05.Size = New System.Drawing.Size(67, 35)
+        Me.lblOutput05.TabIndex = 6
+        Me.lblOutput05.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblOutput06
+        '
+        Me.lblOutput06.BackColor = System.Drawing.Color.Gold
+        Me.lblOutput06.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput06.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOutput06.Location = New System.Drawing.Point(574, 187)
+        Me.lblOutput06.Name = "lblOutput06"
+        Me.lblOutput06.Size = New System.Drawing.Size(67, 35)
+        Me.lblOutput06.TabIndex = 7
+        Me.lblOutput06.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(564, 129)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(85, 28)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "CashBall"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(770, 249)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblOutput06)
+        Me.Controls.Add(Me.lblOutput05)
+        Me.Controls.Add(Me.lblOutput04)
+        Me.Controls.Add(Me.lblOutput03)
+        Me.Controls.Add(Me.lblOutput02)
+        Me.Controls.Add(Me.lblOutput01)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnQuickPick)
+        Me.Name = "Form1"
+        Me.Text = "CashBall - Win $1,000 a Day 4 Life!"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnQuickPick As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblOutput01 As System.Windows.Forms.Label
+    Friend WithEvents lblOutput02 As System.Windows.Forms.Label
+    Friend WithEvents lblOutput03 As System.Windows.Forms.Label
+    Friend WithEvents lblOutput04 As System.Windows.Forms.Label
+    Friend WithEvents lblOutput05 As System.Windows.Forms.Label
+    Friend WithEvents lblOutput06 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ Annis_Cash4Life_FA14/Form1.vb
@@ -0,0 +1,70 @@
+﻿Option Strict On
+Public Class Form1
+
+    'Class level variables
+    Dim blnFoundFlag(60) As Boolean             'A boolean array to flag the value of the randomly generated values 
+    Dim objRandom As Random = New Random()      'Object used to randomly generate values
+    Dim intRandomArray(60) As Integer           'Array that holds the value of the random numbers
+
+    Private Sub btnQuickPick_Click(sender As Object, e As EventArgs) Handles btnQuickPick.Click
+        'This is the button click event that executes when the user clicks the Quick Pick button.
+
+        'Local variables
+        Dim intCount As Integer         'Holds count during loop and used to assign values to appropriate position in array
+        Dim intUpper As Integer         'Used to hold the value of the upper bound
+
+        'The For Loop loops five times and assigns the value of the RandomNumber function to the intRandomArray 
+        For intCount = 0 To 4
+
+            'Sets the upper bound number so that only numbers 1 through 60 will be generated
+            intUpper = 61
+
+            'The RandomNumber function is called and the value returned is assigned to the position in the Array.
+            'The position in the array is decided by the count in the For Loop.
+            intRandomArray(intCount) = RandomNumber(intUpper)
+        Next
+
+        'This code resets the FoundFlag boolean to allow the Cashball number to be a number that had previously been picked
+        ReDim blnFoundFlag(60)
+
+        'This changes the upper bound number to only allow numbers from 1 to 4 for the Cashball number
+        intUpper = 5
+
+        'The RandomNumber function is called and the value returned is assigned to the sixth position in the Array
+        intRandomArray(5) = RandomNumber(intUpper)
+
+        'The following code outputs the values of the randomly generated numbers 
+        lblOutput01.Text = intRandomArray(0).ToString
+        lblOutput02.Text = intRandomArray(1).ToString
+        lblOutput03.Text = intRandomArray(2).ToString
+        lblOutput04.Text = intRandomArray(3).ToString
+        lblOutput05.Text = intRandomArray(4).ToString
+        lblOutput06.Text = intRandomArray(5).ToString
+
+    End Sub
+
+
+    Function RandomNumber(ByVal intUpper As Integer) As Integer
+        'This function generates a random number and returns the value. The random number generated is verified to 
+        'be a unique number before being returned.
+
+        'Local variable
+        Dim intNum As Integer       'holds the value of the random number generated
+
+        'This generates the random number and verifies the number is unique before exiting the loop
+        Do
+            'This is the code that generates the random number within the range of 1 to 60 and assigns it to intNum
+            intNum = objRandom.Next(1, intUpper)
+
+            'This verifies if the random number generated is unique. If the value of the number is already set to true,
+            'then it loops back and generates a new number until it finds a number that has not been flagged yet.
+        Loop Until blnFoundFlag(intNum) = False
+
+        'This flags the random number generated for the next time through the loop
+        blnFoundFlag(intNum) = True
+
+        'returns the value of the random number generated
+        Return intNum
+
+    End Function
+End Class
